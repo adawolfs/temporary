dde13a5a6931098ae73d79c39a44a35cbcc0fbf5
adawolfs <adawolfs@gmail.com>
2024-09-28 02:20:23 +0000

    Deploying to gh-pages from @ adawolfs/temporary@a6bfe240c1c378665e9317a9ed20d3cf55fc7522 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
-"flutter_bootstrap.js": "2056ee6bd316352af900b0f1f8def6ce",
+"flutter_bootstrap.js": "bcc40cf9311648eb6ff51fd2dd673bc1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "index.html": "00bd717ba1e53f3e8dab23d83df365bd",
